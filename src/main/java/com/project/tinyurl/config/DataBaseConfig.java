32fc33b2cb251c4bd2f301bc03266def902761a6
Aravind Sundarraj <aravind.ssr@gmail.com>
2022-06-21 14:11:25 -0400

    commit
    commit

--- src/main/java/com/project/tinyurl/config/DataBaseConfig.java
+++ src/main/java/com/project/tinyurl/config/DataBaseConfig.java
@@ -2,15 +2,21 @@ package com.project.tinyurl.config;
 
 import com.zaxxer.hikari.HikariDataSource;
 import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.Jedis;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DataBaseConfig {
+
+    @Value("${tinyurl.redis.conn}")
+    private String redisConn;
 
 
     @Bean
@@ -26,6 +32,13 @@ public class DataBaseConfig {
     public DataSource dataSource(){
 
         return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    public Jedis jedis(){
+        Jedis jedis = new Jedis(redisConn);
+        jedis.auth("","");
+        return jedis;
     }
 
 
